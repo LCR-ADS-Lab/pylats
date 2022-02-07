1e8d38a4219908b5558dc6234155398835b9b152
Kristopher Kyle <kristopherkyle@Kristophers-MacBook-Pro.local>
2022-02-07 07:25:13 -0800

    upload latest version to pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pylats",
-    version="0.23",
+    version="0.24",
     author="Kristopher Kyle",
     author_email="kristopherkyle1@gmail.com",
     description="Text preprocessing for downstream linguistic analyses",
